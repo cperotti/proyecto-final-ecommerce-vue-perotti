e7ba763655a850247f18987dcf75058463a0057e
cperotti <carolinaperotti16@gmail.com>
2023-01-29 18:58:10 -0300

    not found view

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,15 +1,32 @@
 <template>
 <section class="section-detail">
   <div class="container">
-   <h1>No se encontró la pagina</h1>
+    <div class="card mb-3">
+  <img src="https://img.freepik.com/vector-gratis/error-404-ilustracion-concepto-paisaje_114360-7898.jpg?w=2000" class="card-img-top image" alt="no encontrado">
+  <div class="card-body d-flex justify-content-center align-items-center flex-column">
+      <h1>Oops!</h1>
+      <h2>🌳GardenSHOP🌳</h2>
+      <p>No ha encontrado la pagina que estas buscando</p>
+  </div>
+</div>
   </div>
 </section>
 </template>
 
 <script>
-// @ is an alias to /src
-
 export default {
   name: 'NotFoundView',
 }
 </script>
+
+<style scoped>
+.section-detail{
+   padding: 24px;
+  width: 100%;
+  height: 100%;
+}
+.image{
+  object-fit: cover;
+  height: 500px;
+}
+</style>
